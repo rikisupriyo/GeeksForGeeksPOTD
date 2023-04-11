68f54d391205804a0211a39a3a55453d5ef3b8ff
Supriyo Ain <100848060+rikisupriyo@users.noreply.github.com>
2023-04-11 13:13:52 +0530

    Update MaximumLength.cpp

--- MaximumLength.cpp
+++ MaximumLength.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solve(int a, int b, int c) {
-        if (a <= 2*(b+c) && b <= 2*(c+a) && c <= 2*(a+b))
+        if (a <= 2*(b+c+1) && b <= 2*(c+a+1) && c <= 2*(a+b+1))
             return a+b+c;
         return -1;
     }
